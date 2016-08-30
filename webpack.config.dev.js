ad77bd4205bc81418fce695b3415715857611ce0
Marcos Abreu <abreu.marcos@gmail.com>
2016-08-30 09:38:29 -0400

    add webpack config for dev and prod

---
+++ webpack.config.dev.js
@@ -0,0 +1,83 @@
+import webpack from 'webpack';
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const GLOBALS = {
+  'process.env.NODE_ENV': JSON.stringify('development'),
+  __DEV__: true
+};
+
+export default {
+  debug: true,
+  devtool: 'cheap-module-eval-source-map', // more info: https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
+  noInfo: true, // set to false to see a list of every file being bundled.
+  entry: [
+    'babel-polyfill',
+    'webpack-hot-middleware/client?reload=true',
+    './src/index'
+  ],
+  target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
+  output: {
+    path: `${__dirname}/src`, // Note: Physical files are only output by the production build task `npm run build`.
+    publicPath: 'http://localhost:5000/', // Use absolute paths to avoid the way that URLs are resolved by Chrome when they're parsed from a dynamically loaded CSS blob. Note: Only necessary in Dev.
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].chunk.js'
+  },
+  plugins: [
+    new webpack.DefinePlugin(GLOBALS), // Tells React to build in dev mode.
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new HtmlWebpackPlugin({  // Create HTML file that includes references to bundled CSS and JS.
+      template: 'src/assets/index.ejs',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      },
+      inject: true
+    })
+  ],
+  module: {
+    loaders: [
+      { // json
+        test: /\.json$/,
+        loader: "json-loader"
+      },
+
+      { // scripts
+        test: /\.js$/,
+        include: path.join(__dirname, 'src'),
+        loaders: ['babel']
+      },
+      { // fonts - EOT format
+        test: /\.eot(\?v=\d+.\d+.\d+)?$/,
+        loader: 'file-loader?name=fonts/[name].[ext]'
+      },
+      { // fonts - WOOF format
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]"
+      },
+      { // fonts - TTF format
+        test: /\.ttf(\?v=\d+.\d+.\d+)?$/,
+        loader: 'file-loader?limit=10000&mimetype=application/octet-stream&name=fonts/[name].[ext]'
+      },
+      { // fonts - SVG format
+        test: /\.svg(\?v=\d+.\d+.\d+)?$/,
+        include: path.join(__dirname, 'src', 'assets', 'fonts'),
+        loader: 'file-loader?limit=10000&mimetype=image/svg+xml&name=fonts/[name].[ext]'
+      },
+      { // images
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        include: path.join(__dirname, 'src'),
+        exclude: path.join(__dirname, 'src', 'assets', 'fonts'),
+        loaders: ['file-loader?name=images/[name].[ext]']
+      },
+      { // ico files - usually just for the favicon.ico
+        test: /\.ico$/, loader: 'file-loader?name=[name].[ext]'
+      },
+      { // css files
+        test: /(\.css|\.scss)$/,
+        loaders: ['style', 'css?sourceMap', 'sass?sourceMap']
+      }
+    ]
+  }
+};
---
+++ webpack.config.prod.js
@@ -0,0 +1,112 @@
+// For info about this file refer to webpack and webpack-hot-middleware documentation
+// For info on how we're generating bundles with hashed filenames for cache busting: https://medium.com/@okonetchnikov/long-term-caching-of-static-assets-with-webpack-1ecb139adb95#.w99i89nsz
+import webpack from 'webpack';
+import path from 'path';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const GLOBALS = {
+  'process.env.NODE_ENV': JSON.stringify('production'),
+  __DEV__: false
+};
+
+export default {
+  debug: true,
+  devtool: 'source-map', // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
+  noInfo: true, // set to false to see a list of every file being bundled.
+  entry: [
+    'babel-polyfill',
+    './src/index'
+  ],
+  target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
+  output: {
+    path: `${__dirname}/dist/`,
+    publicPath: '/',
+    filename: 'js/[name].[chunkhash].js',
+    chunkFilename: 'js/[name].[chunkhash].js',
+  },
+  plugins: [
+    // Hash the files using MD5 so that their names change when the content changes.
+    new WebpackMd5Hash(),
+
+    // Optimize the order that items are bundled. This assures the hash is deterministic.
+    new webpack.optimize.OccurenceOrderPlugin(),
+
+    // Tells React to build in prod mode. https://facebook.github.io/react/downloads.html
+    new webpack.DefinePlugin(GLOBALS),
+
+    // Generate an external css file with a hash in the filename
+    new ExtractTextPlugin('css/[name].[contenthash].css'),
+
+    // Generate HTML file that contains references to generated bundles. See here for how this works: https://github.com/ampedandwired/html-webpack-plugin#basic-usage
+    new HtmlWebpackPlugin({
+      template: 'src/assets/index.ejs',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true,
+      // Note that you can add custom options here if you need to handle other custom logic in index.html
+      // To track JavaScript errors via TrackJS, sign up for a free trial at TrackJS.com and enter your token below.
+      trackJSToken: ''
+    }),
+
+    // Eliminate duplicate packages when generating bundle
+    new webpack.optimize.DedupePlugin(),
+
+    // Minify JS
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [
+      { // json
+        test: /\.json$/,
+        loader: "json-loader"
+      },
+      { // scripts
+        test: /\.js$/,
+        include: path.join(__dirname, 'src'),
+        loaders: ['babel']
+      },
+      { // fonts - EOT format
+        test: /\.eot(\?v=\d+.\d+.\d+)?$/,
+        loader: 'file-loader?name=fonts/[name].[ext]'
+      },
+      { // fonts - WOOF format
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]"
+      },
+      { // fonts - TTF format
+        test: /\.ttf(\?v=\d+.\d+.\d+)?$/,
+        loader: 'file-loader?limit=10000&mimetype=application/octet-stream&name=fonts/[name].[ext]'
+      },
+      { // fonts - SVG format
+        test: /\.svg(\?v=\d+.\d+.\d+)?$/,
+        include: path.join(__dirname, 'src', 'assets', 'fonts'),
+        loader: 'file-loader?limit=10000&mimetype=image/svg+xml&name=fonts/[name].[ext]'
+      },
+      { // images
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        include: path.join(__dirname, 'src'),
+        exclude: path.join(__dirname, 'src', 'assets', 'fonts'),
+        loaders: ['file-loader?name=images/[name].[ext]']
+      },
+      { // ico files - usually just for the favicon.ico
+        test: /\.ico$/, loader: 'file-loader?name=[name].[ext]'
+      },
+      { // css files
+        test: /(\.css|\.scss)$/,
+        loader: ExtractTextPlugin.extract('css?sourceMap!sass?sourceMap')
+      }
+    ]
+  }
+};
