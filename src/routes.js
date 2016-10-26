3e3ff3fbfff8444415f8ac11a04c1206a32333cb
Marcos Abreu <abreu.marcos@gmail.com>
2016-10-25 20:29:57 -0400

    fix import js path issue

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './containers/app';
 import HomePage from './components/home-page';
-import PageNotFound from './components/page-not-found.js';
+import PageNotFound from './components/page-not-found';
 
 export default (
   <Route path="/" component={App}>
